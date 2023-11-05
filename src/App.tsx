a3f2dcd5ca7d8def83fb6b1c7011d8e66dd08c2d
alajido <96017433+Alajido@users.noreply.github.com>
2023-11-05 08:33:17 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-[#172554] h-screen">
+    <h1 className="text-4xl text-center text-white">welcome to the kano devfest student edition website</h1>
+
+    </div>
+  )
+}
+
+export default App
